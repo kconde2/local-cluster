86b938c91db606d0a98f49130dc8d25e4ffa7bc5
Kaba CONDE <kabaconde15@gmail.com>
2021-06-22 02:07:43 +0200

    feat: add mysql and update installation

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ kind create cluster --config ./kind.config.yaml
 # References
 
 - https://docs.k8ssandra.io/tasks/connect/ingress/kind-deployment/
+- https://github.com/stakater/Reloader
--- traefik.sh
+++ traefik.sh
@@ -4,3 +4,11 @@ helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
 helm install traefik traefik/traefik -n traefik --create-namespace -f traefik.values.yaml
 # helm upgrade traefik traefik/traefik -n traefik --values traefik.values.yaml
+
+kubectl create ns uat
+kubectl create ns prod
+kubectl apply -f traefik.dashboard.yaml
+kubectl apply -f mysql.deployment.yaml
+kubectl apply -f mysql.pv.yaml
+kubectl apply -f mysql.service.yaml
+kubectl apply -f https://raw.githubusercontent.com/stakater/Reloader/master/deployments/kubernetes/reloader.yaml
